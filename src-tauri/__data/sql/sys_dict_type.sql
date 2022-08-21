1b9902a476c10a2567593d83c4d941493f6c86ea
lingdu <waong2005@126.com>
2022-08-21 21:26:16 +0800

    update db

---
+++ src-tauri/__data/sql/sys_dict_type.sql
@@ -0,0 +1,7 @@
+INSERT INTO "sys_dict_type" VALUES ('00UHHG0BSSUOETFAOH17300Q6R', '菜单状态', 'sys_show_hide', 1, '菜单状态');
+INSERT INTO "sys_dict_type" VALUES ('00UHHG6K1346UE9P05BTTVS8V7', '系统开关', 'sys_normal_disable', 1, '系统开关');
+INSERT INTO "sys_dict_type" VALUES ('00UHHHTTHGVTTUEQDME7KMLM6T', '系统状态', 'sys_common_status', 1, '系统状态');
+INSERT INTO "sys_dict_type" VALUES ('010EAVCOK691DPFUAOKDRE98RK', '仪器名称', 'instrumentNames', 1, '仪器名称');
+INSERT INTO "sys_dict_type" VALUES ('010F1C5VRUKO1P5RET28A9DS0J', 'MC项目组', 'mc_test_group', 1, 'MC项目组');
+INSERT INTO "sys_dict_type" VALUES ('010GTPIEB3JQ9PMFAI2SMHNQS1', 'MC样本类型', 'mc_sample_type', 1, 'MC样本类型');
+INSERT INTO "sys_dict_type" VALUES ('010GTS8V9GJHHL71JT2EOH3VVP', 'MC样本状态', 'mc_sample_status', 1, 'MC样本状态');
